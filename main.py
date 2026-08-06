e354a5205343d9e3cd4c7571a8a855e382521242
Garcia <davignoronha25@gmail.com>
2026-08-06 11:01:00 -0300

    arquivos

---
+++ main.py
@@ -0,0 +1,35 @@
+def menu_principal():
+    while True:
+    print("-------Menu Principal-------")
+    print("1 - CADASTRAR")
+    print("2 - EMPRESTAR")
+    print("3 - DEVOLUÇÃO")
+    print("4 - LISTAR")
+    print("5 - BUSCAR")
+    print("6 - ORDENAR")
+    print("7 - SAIR")
+    opcao = int(input("Digite a opção escolhida: "))
+    if opcao == 1:
+        d.cadastrar()
+    elif opcao == 2:
+    d.emprestar()
+    elif opcao == 3:
+    d.devolucao()
+    elif opcao == 4:
+    d.listar()
+    elif
+    
+
+
+    
+
+
+        
+            
+
+                    
+
+            
+                    
+                    
+                
